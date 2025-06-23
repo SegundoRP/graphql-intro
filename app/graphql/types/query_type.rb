ff5f772178eed9e05b5d42b2dbe3e5439272ab39
Segundo Rebaza <alvaro.rp1593@gmail.com>
2025-06-22 23:33:56 -0500

    chore: add types for models

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -2,30 +2,21 @@
 
 module Types
   class QueryType < Types::BaseObject
-    field :node, Types::NodeType, null: true, description: "Fetches an object given its ID." do
-      argument :id, ID, required: true, description: "ID of the object."
+    field :posts, [Types::PostType], null: false do
+      description 'Query that selects all posts'
     end
 
-    def node(id:)
-      context.schema.object_from_id(id, context)
+    field :postsCount, Integer, null: false do
+      description 'Query that returns the total number of posts'
     end
 
-    field :nodes, [Types::NodeType, null: true], null: true, description: "Fetches a list of objects given a list of IDs." do
-      argument :ids, [ID], required: true, description: "IDs of the objects."
+    def posts
+      return Post.all
     end
 
-    def nodes(ids:)
-      ids.map { |id| context.schema.object_from_id(id, context) }
+    def postsCount
+      return Post.count
     end
 
-    # Add root-level fields here.
-    # They will be entry points for queries on your schema.
-
-    # TODO: remove me
-    field :test_field, String, null: false,
-      description: "An example field added by the generator"
-    def test_field
-      "Hello World!"
-    end
   end
 end
