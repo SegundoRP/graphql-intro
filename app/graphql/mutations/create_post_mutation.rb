44cfaa1ab7133035ed14d432ddd5e72c72ae6b0f
Segundo Rebaza <alvaro.rp1593@gmail.com>
2025-06-24 00:38:02 -0500

    feat: add post mutation

--- app/graphql/mutations/create_post_mutation.rb
+++ app/graphql/mutations/create_post_mutation.rb
@@ -2,15 +2,32 @@
 
 module Mutations
   class CreatePostMutation < BaseMutation
-    # TODO: define return fields
-    # field :post, Types::PostType, null: false
+    field :post, Types::PostType, null: false
 
-    # TODO: define arguments
-    # argument :name, String, required: true
+    argument :title, String, required: true do
+      description "The title field is a string type and is required for a new Post to be created."
+    end
 
-    # TODO: define resolve method
-    # def resolve(name:)
-    #   { post: ... }
-    # end
+    argument :body, String, required: true do
+      description "This is the post's main content, it is called the body and it expects a string"
+    end
+
+    argument :author, Integer, required: true do
+      description "The id of this post's author."
+    end
+
+    def resolve(title:, body:, author:)
+      @post = Post.new(title: title, body: body, author: Author.find_by_id(author))
+
+      if (@post.save)
+        {
+          post: @post,
+          errors: []
+        } else {
+          post: nil,
+          errors: @post.errors.full_messages
+        }
+      end
+    end
   end
 end
